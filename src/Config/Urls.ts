72b36403a59f96e0d5724d6d7462152fbec36a1f
Krishna-Arjun <krishnaarjunkl@gmail.com>
2024-11-19 03:01:29 +0530

    updated the base url

--- src/Config/Urls.ts
+++ src/Config/Urls.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://task-manger-server-66o6.onrender.com";
-export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL = "https://task-manger-server-66o6.onrender.com";
+// export const BASE_URL = "http://localhost:8080/";
