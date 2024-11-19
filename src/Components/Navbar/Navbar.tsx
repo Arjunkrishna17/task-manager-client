6d7b6857dacace6f9dbe0adc642fb4abf060db73
Krishna-Arjun <krishnaarjunkl@gmail.com>
2024-11-19 06:05:15 +0530

    project name

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -8,13 +8,23 @@ const Navbar = () => {
 
   return (
     <div className="flex w-full h-16 items-center px-5 sm:px-16 justify-between bg-blue-900">
-      <div className="flex space-x-2 items-center">
-        <img src="Images/Task.svg" alt="Task manager" width={40} height={40} />
+      <a
+        className="cursor-pointer"
+        href="https://task-manager-krishna.netlify.app/"
+      >
+        <div className="flex space-x-2 items-center">
+          <img
+            src="Images/Task.svg"
+            alt="Task manager"
+            width={40}
+            height={40}
+          />
 
-        <h3 className="text-xl font-semibold text-blue-50 mt-1">
-          Task Manager
-        </h3>
-      </div>
+          <h3 className="text-xl font-semibold text-blue-50 mt-1">
+            Task Manager
+          </h3>
+        </div>
+      </a>
 
       <div className="flex space-x-5 items-center ">
         {isAuthenticated && (
