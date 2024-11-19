6dae8685c4be336b44107ac5f1770c26a55db5f1
Krishna-Arjun <krishnaarjunkl@gmail.com>
2024-11-19 08:15:20 +0530

    card space

--- src/Components/DND/DragableColumn.tsx
+++ src/Components/DND/DragableColumn.tsx
@@ -88,7 +88,7 @@ const DraggableColumns: React.FC = () => {
                   <div
                     ref={provided.innerRef}
                     {...provided.droppableProps}
-                    className="flex flex-col p-4 w-full sm:w-72 md:w-1/3 lg:w-1/4 bg-white rounded-lg shadow-md"
+                    className="flex flex-col p-4 w-full sm:w-72 md:w-1/3 lg:w-1/4 bg-white rounded-lg shadow-md space-y-2"
                   >
                     <h3 className="w-full bg-blue-800 text-white px-2 py-1 mb-3 text-center">
                       {column.title}
