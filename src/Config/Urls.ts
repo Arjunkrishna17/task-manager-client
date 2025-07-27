c89d9626155ff28dbcfb584da7ee237e82495b8a
Krishna-Arjun <krishnaarjunkl@gmail.com>
2025-07-27 11:05:25 +0530

    new server url

--- src/Config/Urls.ts
+++ src/Config/Urls.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.REACT_APP_ENV === "dev"
     ? "http://localhost:8080"
-    : "https://task-manager-api.krishnadas.site";
+    : "https://continued-loutitia-shelf-686a6c4e.koyeb.app";
 
 export const HOME_PAGE =
   process.env.REACT_APP_ENV === "dev"
